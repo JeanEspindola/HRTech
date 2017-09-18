e3a96624ead1f86eb564965bcc631fbd84415230
JeanEspindola <jean.espindola@gmail.com>
2017-09-17 22:37:45 -0300

    Adding constant.

---
+++ app/core/js/constants/packages-constants.js
@@ -0,0 +1,50 @@
+/**
+ * Constant for holding static values for all packages card.
+ *
+ **/
+(function() {
+    'use strict';
+    angular
+        .module('hrtech.core')
+        .constant('hrtech.core.packageConstant', {
+/*
+
+
+            Single Attandee
+            Group Tickets
+            Investor
+                Startups
+
+            Early Bird Soldout / Summer Saver {icone} until aug31 / Regular / Late / Onsite registration
+
+
+    Fashion axe keytar truffaut migas. Farm-to-table PBR&B. Drinking vinegar sustainable helvetica sartorial. Dreamcatcher live-edge lo-fi, chartreuse echo park pinterest ditilley glossier plaid fingerstache. Fashion axe keytar truffaut migas. Farm-to-table PBR&B. Drinking vinegar sustainable helvetica
+
+    Cold-pressed poke thundercats brooklyn chillwave chartreuse
+    Craft beer 3 wolf moon tbh hoodie
+    YOLO synth hammock
+    Distilery aesthetic VHS affogato gentrify bespoke
+    Chia readymade schlitz brunch yuccie echo park
+
+    1. 	.Summer saver
+    .1595
+        .Save 300
+        .Until August 31
+
+    2	.Expo Only
+    .299
+        .Until October
+
+    3   .Public & Governmental Sector
+    .1295
+        .Save 300 from the Summer Saver
+        .Until October
+
+    4	.Standard Conference & Expo
+    .1695
+        .Do you have a promo code?
+    .Type here & then pick DAY01/DAY02 OR BOTH
+*/
+
+})
+})();
